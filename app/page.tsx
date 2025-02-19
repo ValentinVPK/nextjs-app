cc18f2822b713e9d6bb27450e0207a4ed56cf07f
ValentinVPK <valentinvpk@abv.bg>
2025-02-19 16:43:17 +0200

    Begin with meals project

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/logo.png"
-        alt="A server surrounded by magic sparkles"
-        width={200}
-        height={200}
-      />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
